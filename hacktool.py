7c017922041c1a66c4cbb215e440b3ee2a10975a
NF-attacker <59114717+nazimtch@users.noreply.github.com>
2021-10-30 23:29:36 +0200

    Add files via upload

---
+++ hacktool.py
@@ -0,0 +1,9 @@
+password = input ("ENTER THE FIRST PASSWORD USED TO OPEN HACK-LAB TOOL : ")
+if password == ("hashmd5tryagainn") :
+    print ("DONE ..!")
+    import os 
+    os.system ("apt install bash")
+    os.system ("git clone https://github.com/Z4nzu/hackingtool")
+    os.system ("bash hacktool.sh")
+else  :
+    print ("WRONG PASSWORD PLEASE CONTACT THE CODDER OF THIS TOOL HE WILL GIVE YOU THE PASSWORD ")
