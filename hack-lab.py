b2e38b4aa0da5f4a9f0f1423e661f29f48e38552
NF-attacker <59114717+nazimtch@users.noreply.github.com>
2021-10-30 23:25:23 +0200

    Add files via upload

---
+++ hack-lab.py
@@ -0,0 +1,130 @@
+import os
+os.system ("clear")
+password = input ("ENTER THE PASSWORD TO USE THIS TOOL : ")
+if password == ("hashmd5tryagainn") :
+    print ("WELCOME TO NF-HACK-LAB")
+    print ("NF-HACK-LAB IS A POWERFUL | INTERNAL / EXTERNAL | HACK TOOL")
+    print ("CREATED BY NAZIMTCH | NF-ATTACKER ")
+    print ("TO USE ONLY ON LINUX OS")
+    print ("CREATED USING PYTHON3 LANGUAGE ")
+    print ("INSTAGRAM : https://www.instagram.com/nazimtch")
+    print ("SNAP : nazimtchh")
+    import time
+    time.sleep(9)
+    print ("STARTING NAZIM-PROGRAMMING")
+    time.sleep(1)
+    print ("[---       ]25%")
+    time.sleep(2)
+    print ("[-----     ]50%")
+    time.sleep(2)
+    print ("[--------  ]75%")
+    time.sleep(2)
+    print ("[----------]100%")
+    time.sleep(1)
+    print ("DONE! ENJOY (: ")
+else : 
+    print ("WRONG PASSWORD")
+    print ("CREATED BY NAZIMTCH TEAM | PLEASE CONTACT THE CODER")
+    print ("PLEASE CONTACT US WE WILL SEND YOU THE PASSWORD")
+    print ("INSTAGRAM : https://www.instagram.com/nazimtch")
+    import time
+    time.sleep(8)
+    os.system ("apt install bash")
+    os.system ("bash README.sh")
+ #user interface script username = input ("ENTER YOUR USERNAME : ")
+password2 = input ("ENTER YOUR PASSWORD : ")
+if password2 == ("hashmd5tryagainn") :
+    username = input ("PLEASE  ENTER ANY USERNAME TO USE  : ")
+    import time
+    time.sleep(3)
+    print ("HI USER " " " +username )
+    print ("ENJOY (: ")
+    time.sleep(3)
+    import os
+    os.system ("clear")
+#HACK-LAB SCRIPT
+    print ("OUR PENETRATION SERVICE")
+    print ("1 ------ : INFORMATION-GATHERING TOOLS")
+    print ("2 ------ : PASSWORDS-ATTACK")
+    print ("3 ------ : SERVER-ATTACK ") 
+    print ("4 ------ : CAMERA-HACKING ")
+    print ("5 ------ :KOUKOU VAGABON  ALL CYBER-ATTACKS TYPE TOOL ")
+
+    option = input ("PLEASE CHOOSE A SERVICE : ")
+    time.sleep(4)
+    os.system ("clear")
+#if choice script
+    if option == ("1") :
+        print ("A ------ : NF-IP-FOUNDER")
+        print ("B ------ :NF-GATHERING")
+        print ("C ------ :NF-GATHERING V 2.0")
+        print ("D ------ :NMAP")
+        choice = input ("CHOOSE A TOL TO USE : ")
+        if choice == ("A") :
+            os.system ("clear")
+            print ("NF-IP-FOUNDER IS A TOOL CREATED BY NAZIMTCH ")
+            time.sleep(2)
+            print ("THIS TOOL HELP YOU TO GET ANY IP OF ANNY WEBSITE")
+            print ("THE IP WILL APPEAR EXAMPLE : 192.168.1.1")
+            IP = input (" ENTER THE NAME OF THE WEBSITE : ")
+            os.system ("ping" " " +IP)
+        elif choice == ("B") :
+            import os
+            os.system ("apt install bash")
+            os.system ("python3 nf-gathering.py")    
+        elif choice == ("C") :
+            print ("THIS TOOL WILL BE UPLOAD SOON PLEASE CHECK OTHER SERVICE")
+        elif choice == ("D"):
+            os.system ("apt install nmap")
+            os.system ("nmap --help")
+#2         
+    if option == ("2"):
+        print ("OUR PASSWORD BRUTEFORCE SERVICE : ")
+        print ("A ------ :FACEBOOK ATTACK")
+        print ("B ------ :SNAP ATTACK")
+        print ("C ------ :CUPP TO CREATE A POWERFUL PASSWORD LIST")
+        print ("D ------ :FSOCIETY A POWERFUL TOOL TO BRUTEFORCE ALL WEB SERVICE ")
+        chs = input ("CHOOSE A SERVICE TO USE : ")
+        if chs == ("A") :
+            import os 
+            os.system ("python3 fb.py")
+        elif chs == ("B") :
+            print ("WE WILL UPLOAD A SNAPCHAT PASSWORD ATTACK SERVICE SOON")
+        elif chs == ("C") :
+            import os
+            os.system ("python3 cupp.py")
+            os.system ("bash cupp.sh")
+        elif chs == ("D") :
+            os.system ("python3 fsociety.py")
+            os.system ("bash fsociety.sh")
+    if option == ("3") :
+        print ("THER IS ONLY ONE  TOOL THAT WILL KILL ANY WEB SERVER")
+        import time
+        time.sleep(2)
+        print ("A ------ : DOS-ATTACK ")
+        chss = input ("TYPE A TO START DOS-ATTACK : ")
+        if chss == ("A") :
+            import os 
+            os.system ("python3 dos.py")
+            os.system ("apt install bash")
+            os.system ("bash dos.sh")
+    if option == ("4") :
+        import os 
+        os.system ("python3 cam.py")
+        os.system ("apt install bash")
+        os.system ("bash cam.sh")
+    if option == ("5") :
+        print ("INSTALLING KOUKOU VAGABON A POWERFUL HACK TOOL | NF-ATTACKER X NAZIMTCH")
+        import time 
+        time.sleep(2)
+        print (" 25%")
+        time.sleep(3)
+        print ("50%")
+        time.sleep(3)
+        print ("75%")
+        time.sleep(3)
+        print ("100%")
+        print ("DONE --- ENJOY IT ")
+        import os
+        os.system ("python3 hacktool.py")
+        os.system ("bash hacktool.py")
