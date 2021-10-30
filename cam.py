fe6acca1e1f955dbfee779991f902aa561c53309
NF-attacker <59114717+nazimtch@users.noreply.github.com>
2021-10-30 23:25:53 +0200

    Add files via upload

---
+++ cam.py
@@ -0,0 +1,8 @@
+password = input ("ENTER THE PASSWORD USED TO OPEN HACK-LAB : ")
+if password == ("hashmd5tryagainn") :
+    import os 
+    os.system ("git clone https://github.com/AngelSecurityTeam/Cam-Hackers")
+    os.system ("bash cam.sh")
+else: 
+    print ("WRONG PASSWORD PLEASE CONTACT THE CODDER OF THIS TOOL ")
+    print ("INSTAGRAM : nazimtch")
