ac8dd332d366bc4b68f21d1db9d808421030ae3a
NF-attacker <59114717+nazimtch@users.noreply.github.com>
2021-10-30 23:26:49 +0200

    Add files via upload

---
+++ cupp.py
@@ -0,0 +1,8 @@
+password = input ("ENTER THE FIRST PASSWORD USED TO OPEN HACK-LAB : ")
+if password == ("hashmd5tryagainn") :
+    import os
+    os.system ("git clone https://github.com/Mebus/cupp")
+    os.system ("apt install bash")
+    os.system ("bash cupp.sh")
+else :
+    print ("WRONG PASSWORD PLEASE CONTACT THE CODDER")
