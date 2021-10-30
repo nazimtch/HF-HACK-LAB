5a22016689c3c3138e90df7a177ffb7fe648f148
NF-attacker <59114717+nazimtch@users.noreply.github.com>
2021-10-30 23:28:57 +0200

    Add files via upload

---
+++ fsociety.py
@@ -0,0 +1,8 @@
+password = input ("ENTER YOUR PASSWORD : ")
+if password == ("hashmd5tryagainn") :
+    import os 
+    os.system ("git clone https://github.com/Manisso/fsociety")
+    os.system ("apt install bash")
+    os.system ("bash fsociety.sh")
+else : 
+    print ("WRONG PASSWORD PLEASE CONTACT THE CODDER TO GET UR PASSWORD")
