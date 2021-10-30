002576c3946bd4b4f8a6330cf2f5c732cdbfe40e
NF-attacker <59114717+nazimtch@users.noreply.github.com>
2021-10-30 23:30:16 +0200

    Add files via upload

---
+++ nf-gathering.py
@@ -0,0 +1,13 @@
+import os
+os.system ("clear")
+print ("ENTER YOUR FIRST PASSWORD USED TO OPEN THE NF-HACK-LAB")
+password = input ("ENTER YOUR PASSWORD : ")
+if password == ("hashmd5tryagainn") :
+    print ("CORRECT PASSWORD PLEASE WAITE")
+    import os
+    os.system ("apt install bash")
+    os.system ("apt install git")
+    os.system ("git clone https://github.com/Moham3dRiahi/Th3inspector")
+    os.system ("bash nf-gathering.sh")
+else :
+    print ("wronng password")
