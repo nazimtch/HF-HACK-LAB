352464b05a27a1f4e419b16319e9bb3aedcaf77d
NF-attacker <59114717+nazimtch@users.noreply.github.com>
2021-10-30 23:28:13 +0200

    Add files via upload

---
+++ fb.py
@@ -0,0 +1,10 @@
+password = input ("ENTER THE FIRST PASSWORD USED TO OPEN HACK-LAB :")
+if password == ("hashmd5tryagainn") :
+    import time
+    time.sleep(2)
+    import os
+    os.system ("apt install bash")
+    os.system ("git clone https://github.com/FR13ND8/BRUTEFORCEnew")
+    os.system ("bash fb.sh")
+else :
+    print ("WRONG PASSWORD PLEASE CONTACT US ")
